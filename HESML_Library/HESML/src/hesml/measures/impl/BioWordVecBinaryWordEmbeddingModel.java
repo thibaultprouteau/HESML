58f4123cf5a9d5f678779ed676c4d778bc84a405
j.lastra <jlastra@invi.uned.es>
2019-10-18 21:47:10 +0200

    I have declared an enum to identify the type of pre-trained word embedding files being evaluated

--- HESML_Library/HESML/src/hesml/measures/impl/BioWordVecBinaryWordEmbeddingModel.java
+++ HESML_Library/HESML/src/hesml/measures/impl/BioWordVecBinaryWordEmbeddingModel.java
@@ -1,18 +1,22 @@
 /*
- * Copyright (C) 2019 j.lastra
+ * Copyright (C) 2016-2020 Universidad Nacional de Educación a Distancia (UNED)
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software for non-commercial use:
+ * you can redistribute it and/or modify it under the terms of the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * (CC BY-NC-SA 4.0) as published by the Creative Commons Corporation,
+ * either version 4 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * section 5 of the CC BY-NC-SA 4.0 License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) 
+ * license along with this program. If not,
+ * see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+ *
  */
 
 package hesml.measures.impl;
@@ -21,19 +25,15 @@ import hesml.measures.IPretrainedWordEmbedding;
 import hesml.measures.IWordSimilarityMeasure;
 import hesml.measures.SimilarityMeasureClass;
 import hesml.measures.SimilarityMeasureType;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
+import hesml.measures.WordEmbeddingFileType;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.text.ParseException;
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
  * This class implements an evaluator of pre-trained BioWordVec
@@ -94,6 +94,18 @@ class BioWordVecBinaryWordEmbeddingModel implements IWordSimilarityMeasure,
         // We only load those word vectors to be evaluated
         
         loadWordVectorOffsets();
+    }
+    
+    /**
+     * This function returns the type of pre-trained file managed by the
+     * object.
+     * @return 
+     */
+    
+    @Override
+    public WordEmbeddingFileType getWordEmbeddingFileType()
+    {
+        return (WordEmbeddingFileType.BioWordVecBinaryFile);
     }
     
     /**
